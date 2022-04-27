99b5ee0d78e61bfd18b9ee0519346eafdd238ebe
brewcrew059 <mjbrewer059@gmail.com>
2022-04-27 14:34:00 -0400

    plz  commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x4205Cffbc48F47A98454BA5637228C12016853D4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
+
+// Crypto Devs Token Contract Address: 0x0D4F3bdB42aDAa1472ad11689B10AbeFB25AF120
